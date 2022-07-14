f5702ca969f008b60a823e55bdc6c433b252d720
upadhyaykhush <khush.upadhyay@gmail.com>
2022-07-13 19:48:32 -0500

    game partially working

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -13,3 +13,39 @@ document.querySelector('.guess').textContent = 23;
 
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    }
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct number!';
+    }
+    else if (guess > secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'Too High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+        }
+
+
+    }
+    else if (guess < secretNumber) {
+        if (guess > 0) {
+            document.querySelector('.message').textContent = 'Too Low !';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!';
+        }
+
+    }
+});
